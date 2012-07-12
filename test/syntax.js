8243e9429035409a3f81d701606f44c0cfaf3a1f
Christopher Giffard <christopher.giffard@cgiffard.com>
2012-07-12 21:13:50 +1000

    Temporarily added feathers doc. Created syntax test runner js file.

---
+++ test/syntax.js
@@ -0,0 +1,43 @@
+// Checks compilation of individial testfiles matches expectations
+
+var chai	= require("chai"),
+	fs		= require("fs");
+
+// Init Chai object sugar
+var expect = chai.expect;
+
+describe("Syntax test",function() {
+	
+	var dirContents = fs.readdirSync("./test/syntax");
+		
+	var syntaxTests = dirContents.filter(function(item) {
+			return item.match(/\.dd$/);
+		})
+		.map(function(item) {
+			return item.replace(/\.dd$/,"");
+		});
+		
+	syntaxTests.forEach(function(test) {
+		it("'" + test + "' should match reference rendering",function(done) {
+			
+			fs.readFile("./test/syntax/" + test + ".dd",function(error,fileData) {
+				if (error) throw error;
+				
+				var compiledData = "",
+					Duckdown = new (require("../"))();
+				
+				Duckdown.parse(fileData.toString());
+				compiledData = Duckdown.compile();
+				
+				fs.readFile("./test/syntax/" + test + ".html",function(error,referenceData) {
+					if (error) throw error;
+					
+					referenceData = referenceData.toString();
+					expect(referenceData).to.equal(compiledData);
+					
+					done();
+				});
+			});
+		});
+	});
+});
